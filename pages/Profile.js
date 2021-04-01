cef18b3cfc78a3f3526a59b10524cb7d9195b3b5
Jacob Franz <jacobwfranz@gmail.com>
2021-04-01 00:22:07 -0400

    Profile tabs are finally workinggit status!

--- pages/Profile.js
+++ pages/Profile.js
@@ -1,100 +1,150 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FlatList, SafeAreaView, StyleSheet, Text } from 'react-native';
 import Styles from '../components/Styles';
 import ProfileHeader from '../components/profile/ProfileHeader';
 import ProfileButtons from '../components/profile/ProfileButtons';
 
-const ProfileStyles = StyleSheet.create({
-  item: {
-    backgroundColor: '#f9c2ff',
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-    fontSize: 40,
-  },
-});
-
-const testUser = {
-  name: 'Jacob Franz',
-  user: '@Zingsla',
-  genres: ['Rap', 'Rock', 'Alternative', 'Opera', 'Musical Theater', 'Hip-Hop'],
-  bio: 'Just a developer trying to make a cool app! More testing flavor text here...'
-};
-
-const DATA1 = [
-  {
-    id: '1',
-    type: 'header',
-    data: testUser,
-  },
-  {
-    id: '2',
-    type: 'buttons',
-  },
-];
-
-const DATA2 = [
-  {
-    id: '3',
-    type: 'text',
-  },
-  {
-    id: '4',
-    type: 'text',
-  },
-  {
-    id: '5',
-    type: 'text',
-  },
-  {
-    id: '6',
-    type: 'text',
-  },
-  {
-    id: '7',
-    type: 'text',
-  },
-  {
-    id: '8',
-    type: 'text',
-  },
-  {
-    id: '9',
-    type: 'text',
-  },
-  {
-    id: '10',
-    type: 'text',
-  },
-];
-
-const profileItem = ({ item }) => {
-  if (item.type === 'header')
-    return <ProfileHeader data={item.data} />;
-  else if (item.type === 'buttons')
-    return <ProfileButtons />;
-  else if (item.type === 'text') {
-    return <Text style={ProfileStyles.item}>TESTING TEXT</Text>;
-  }
-};
-
-const footerItem = () => {
-  return (
-    <FlatList
-      data={DATA2}
-      renderItem={profileItem}
-    />
-  );
-};
-
 const Profile = () => {
+  const ProfileStyles = StyleSheet.create({
+    item: {
+      backgroundColor: '#f9c2ff',
+      padding: 20,
+      marginVertical: 8,
+      marginHorizontal: 16,
+      fontSize: 40,
+    },
+  });
+
+  const testUser = {
+    name: 'Jacob Franz',
+    user: '@Zingsla',
+    genres: ['Rap', 'Rock', 'Alternative', 'Opera', 'Musical Theater', 'Hip-Hop'],
+    bio: 'Just a developer trying to make a cool app! More testing flavor text here...'
+  };
+
+  const DATA1 = [
+    {
+      id: '1',
+      type: 'header',
+      data: testUser,
+    },
+    {
+      id: '2',
+      type: 'buttons',
+    },
+    {
+      id: '3',
+      type: 'text',
+    },
+    {
+      id: '4',
+      type: 'text',
+    },
+    {
+      id: '5',
+      type: 'text',
+    },
+    {
+      id: '6',
+      type: 'text',
+    },
+    {
+      id: '7',
+      type: 'text',
+    },
+    {
+      id: '8',
+      type: 'text',
+    },
+    {
+      id: '9',
+      type: 'text',
+    },
+    {
+      id: '10',
+      type: 'text',
+    },
+  ];
+
+  const DATA2 = [
+    {
+      id: '1',
+      type: 'header',
+      data: testUser,
+    },
+    {
+      id: '2',
+      type: 'buttons',
+    },
+    {
+      id: '3',
+      type: 'text2',
+    },
+    {
+      id: '4',
+      type: 'text2',
+    },
+    {
+      id: '5',
+      type: 'text2',
+    },
+    {
+      id: '6',
+      type: 'text2',
+    },
+    {
+      id: '7',
+      type: 'text2',
+    },
+    {
+      id: '8',
+      type: 'text2',
+    },
+    {
+      id: '9',
+      type: 'text2',
+    },
+    {
+      id: '10',
+      type: 'text2',
+    },
+  ];
+  
+  const [refresh, setRefresh] = useState(false);
+  const [profileData, setData] = useState(DATA1);
+
+  let selectedIndex = 0;
+
+  let updateIndex = (newIndex) => {
+    selectedIndex = newIndex;
+    console.log('Its working??');
+    
+    if (selectedIndex === 0)
+      setData(DATA1);
+    else if (selectedIndex === 1)
+      setData(DATA2);
+    
+    setRefresh(!refresh);
+  };
+
+  const profileItem = ({ item }) => {
+    if (item.type === 'header')
+      return <ProfileHeader data={item.data} />;
+    else if (item.type === 'buttons')
+      return <ProfileButtons function={updateIndex} />;
+    else if (item.type === 'text')
+      return <Text style={ProfileStyles.item}>TESTING TEXT</Text>;
+    else if (item.type === 'text2')
+      return <Text style={ProfileStyles.item}>TABS ARE WORKING!!!</Text>;
+  };
+
   return (
     <SafeAreaView style={Styles.container, Styles.androidSafeView}>
       <FlatList
-        data={DATA1}
+        data={profileData}
         renderItem={profileItem}
         stickyHeaderIndices={[1]}
-        ListFooterComponent={footerItem}
+        extraData={refresh}
       />
     </SafeAreaView>
   );
