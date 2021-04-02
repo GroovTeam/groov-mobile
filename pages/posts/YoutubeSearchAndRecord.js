a0bb4b99cd77837cd30e45a16f0514431d0ae196
Tyler Hostler-Mathis <tylerhm.dev@gmail.com>
2021-04-02 19:21:18 -0400

    Working recording

--- pages/posts/YoutubeSearchAndRecord.js
+++ pages/posts/YoutubeSearchAndRecord.js
@@ -5,6 +5,7 @@ import NavBar, { NavButton, NavButtonText, NavTitle } from 'react-native-nav';
 import Styles from '../../components/Styles';
 import { Button } from 'react-native-material-ui';
 import NavStyles from '../../components/NavStyles';
+import { Audio } from 'expo-av';
 
 const window = Dimensions.get('window');
 const [windowWidth, windowHeight] = [window.width, window.height];
@@ -41,11 +42,34 @@ const CreatePostStyles = StyleSheet.create({
 
 const CreatePost = ({ doneRecording }) => {
 
-  const [recordActive, setRecordActive] = useState(false);
+  const [recording, setRecording] = useState(undefined);
 
-  const toggleRecord = () => {
-    setRecordActive(!recordActive);
+  const startRecording = async () => {
+    try {
+      console.log('Requesting permissions..');
+      await Audio.requestPermissionsAsync();
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      }); 
+      console.log('Starting recording..');
+      const recording = new Audio.Recording();
+      await recording.prepareToRecordAsync(Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY);
+      await recording.startAsync(); 
+      setRecording(recording);
+      console.log('Recording started');
+    } catch (err) {
+      console.error('Failed to start recording', err);
+    }
   };
+
+  async function stopRecording() {
+    console.log('Stopping recording..');
+    setRecording(undefined);
+    await recording.stopAndUnloadAsync();
+    const uri = recording.getURI(); 
+    console.log('Recording stopped and stored at', uri);
+  }
 
   return (
     <View>
@@ -68,8 +92,8 @@ const CreatePost = ({ doneRecording }) => {
           <Button
             primary
             raised
-            text='Record'
-            onPress={toggleRecord}
+            text={recording ? 'Stop Recording' : 'Start Recording'}
+            onPress={recording ? stopRecording : startRecording}
           />
         </View>
 
