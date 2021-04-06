6d3cca3b76a2d48ed03be5af65a19003f4b50290
Tyler Hostler-Mathis <tylerhm.dev@gmail.com>
2021-04-06 01:25:49 -0400

    More comments

--- utils/getBeats.js
+++ utils/getBeats.js
@@ -2,6 +2,7 @@ import firebase from './Firebase';
 import axios from 'axios';
 import ApiConfig from './ApiConfig';
 
+// Get all registered beats from our servers.
 const getBeats = async () => {
   return firebase.auth().currentUser.getIdToken(/* forceRefresh */ true).then(token => {
 
--- utils/getProfile.js
+++ utils/getProfile.js
@@ -2,6 +2,7 @@ import firebase from './Firebase';
 import axios from 'axios';
 import ApiConfig from './ApiConfig';
 
+// Get the current user's profile.
 const getProfile = async () => {
   return firebase.auth().currentUser.getIdToken(/* forceRefresh */ true).then(token => {
 
